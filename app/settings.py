3af74c4ea8605ba45bc0ba882ff2090d6b704dce
Firework18 <167246791+Firework18@users.noreply.github.com>
2025-05-27 19:20:20 -0500

    Tercer Avance(27/05)

--- app/settings.py
+++ app/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     # local
     "bases",
+    "usuarios",
 ]
 
 MIDDLEWARE = [
@@ -131,3 +132,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = "usuarios.Usuario"
---
+++ usuarios/admin.py
@@ -0,0 +1,36 @@
+from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
+
+from .forms import CustomUserChangeForm, CustomUserCreationForm
+
+UsuarioPersonalizado = get_user_model()
+
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = UsuarioPersonalizado
+
+    list_display = ["email", "username", "is_superuser"]
+
+    # ⚠️ Solución al error con usable_password
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "username",
+                    "email",
+                    "password1",
+                    "password2",
+                    "is_staff",
+                    "is_active",
+                ),
+            },
+        ),
+    )
+
+
+admin.site.register(UsuarioPersonalizado, CustomUserAdmin)
